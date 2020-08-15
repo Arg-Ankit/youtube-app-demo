f3a5bf2868a2c957c097b76218d8e0e82bd24320
ArgAnkit1994 <68583118+ArgAnkit1994@users.noreply.github.com>
2020-08-15 16:11:11 +0530

    Updates

---
+++ precache-manifest.3b70351acc7e4d2144045ba87197a3ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1e295b258340b9d3d329f544f7bff7",
+    "url": "/youtube-app-demo/index.html"
+  },
+  {
+    "revision": "cf1653024ee44da8b462",
+    "url": "/youtube-app-demo/static/css/main.eaf00664.chunk.css"
+  },
+  {
+    "revision": "437121ba4e976bd7cf45",
+    "url": "/youtube-app-demo/static/js/2.9422ef3e.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/youtube-app-demo/static/js/2.9422ef3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf1653024ee44da8b462",
+    "url": "/youtube-app-demo/static/js/main.c8a91431.chunk.js"
+  },
+  {
+    "revision": "af0f789f25b24d17d685",
+    "url": "/youtube-app-demo/static/js/runtime-main.301f5838.js"
+  }
+]);
